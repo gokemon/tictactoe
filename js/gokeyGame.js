64a465f27860216672efe9d3ac38b18665ed6672
gokemon <michael.of.gokey@gmail.com>
2016-04-04 21:49:37 -0400

    cleaned up styling Monday night

--- js/gokeyGame.js
+++ js/gokeyGame.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var gameCounter=0;
     
-
+    // gameloop
     $('#gameBoard').find('.cell').on('click', function(){ // change onclick to one click later
         if (gameCounter % 2 === 0){
             $(this).text('X'); 
@@ -15,13 +15,15 @@ $(document).ready(function(){
         gameCounter++;  
         console.log(gameCounter); 
         if (gameCounter == 9){
-                    $('#winnerIs').text('Game over! a draw');
+                    $('#winnerIs').text('Game over! no winner');
                     $('#newGame').removeClass('invisible');
         }
-
     });// onClick game loop
-     
-    
+    // didn't like the onClick to oneClick, can't reset this????
+
+    /// How do I keep score?
+    /// Would need a place to load player 1 and player 2, and names, etc...
+         
 
     function checkVictory(player){    
         ///top row check
@@ -29,7 +31,7 @@ $(document).ready(function(){
             if ($('#gameBoard').find('#0').text() == $('#gameBoard').find('#1').text()) {
                 if ($('#gameBoard').find('#0').text() == $('#2').text()) {
                     //alert('Game over! '+player+' is the winner!');
-                    $('#winnerIs').text('0-1 Game over! '+player+' is the winner!');
+                    $('#winnerIs').html('0-1 Game over! <br>'+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 }
             }//top row check        
@@ -38,7 +40,7 @@ $(document).ready(function(){
             if ($('#gameBoard').find('#0').text() == $('#gameBoard').find('#3').text()) {
                 if ($('#gameBoard').find('#0').text() == $('#6').text()) {
                     //alert('Game over! '+player+' is the winner!');
-                    $('#winnerIs').text('0-2 Game over! '+player+' is the winner!');
+                    $('#winnerIs').html('0-2 Game over! <br>'+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 }
             }//left column check
@@ -47,31 +49,29 @@ $(document).ready(function(){
             if ($('#gameBoard').find('#0').text() == $('#gameBoard').find('#4').text()) {
                 if ($('#gameBoard').find('#0').text() == $('#8').text()) {
                     //alert('Game over! '+player+' is the winner!');
-                    $('#winnerIs').text('0-3 Game over! '+player+' is the winner!');
+                    $('#winnerIs').html('0-3 Game over! <br>'+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 }
             } //left diagonal check   
-        } // position 1 checks
-        
+        } // position 1 checks    
 
        ///middle column check
         if ($('#gameBoard').find('#1').text() !== ''){
             if ($('#gameBoard').find('#1').text() == $('#gameBoard').find('#4').text()) {
                 if ($('#gameBoard').find('#1').text() == $('#7').text()) {
                     //alert('Game over! '+player+' is the winner!');
-                    $('#winnerIs').text('1-1 Game over! '+player+' is the winner!');
+                    $('#winnerIs').html('1-1 Game over! <br>'+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 } 
             } //middle column check
-        } // position 2 checks
-        
+        } // position 2 checks        
 
         ///right column check
         if ($('#gameBoard').find('#2').text() !== ''){
             if ($('#gameBoard').find('#2').text() == $('#gameBoard').find('#5').text()) {
                 if ($('#gameBoard').find('#2').text() == $('#8').text()) {
                     //alert('Game over! '+player+' is the winner!');
-                    $('#winnerIs').text('2-1 Game over! '+player+' is the winner!');
+                    $('#winnerIs').html('2-1 Game over! <br>'+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 }
             }//right column check
@@ -80,40 +80,34 @@ $(document).ready(function(){
             if ($('#gameBoard').find('#2').text() == $('#gameBoard').find('#4').text()) {
                 if ($('#gameBoard').find('#2').text() == $('#6').text()) {
                     //alert('Game over! '+player+' is the winner!');
-                    $('#winnerIs').text('2-2 Game over! '+player+' is the winner!');
+                    $('#winnerIs').html('2-2 Game over! <br>'+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 }
             }//right diag check
-        } // position 3 checks
-        
+        } // position 3 checks        
 
         ///middle row check        
         if ($('#gameBoard').find('#3').text() !== ''){
             if ($('#gameBoard').find('#3').text() == $('#gameBoard').find('#4').text()) {
                 if ($('#gameBoard').find('#3').text() == $('#5').text()) {
                     //alert('Game over! '+player+' is the winner!');
-                    $('#winnerIs').text('4-1 Game over! '+player+' is the winner!');
+                    $('#winnerIs').html('4-1 Game over! <br>'+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 }
             }//middle row check
         } // position 4 checks
         
-
         ///bottom row check
         if ($('#gameBoard').find('#6').text() !== ''){
             if ($('#gameBoard').find('#6').text() == $('#gameBoard').find('#7').text()) {
                 if ($('#gameBoard').find('#6').text() == $('#8').text()) {
                     //alert('Game over! '+player+' is the winner!');
-                    $('#winnerIs').text('7-1 Game over! '+player+' is the winner!');
+                    $('#winnerIs').html('7-1 Game over! <br>'+player+' is the winner!');
                     $('#newGame').removeClass('invisible');
                 }
             }//bottom row check
         } // position 6 checks
-
-
     }///checkVictory
-    // do I want to add an outer loop that says, if all spaces were taken and no win was found that its a draw game?
-    //a simple if
 
 
     /// display new game
@@ -122,6 +116,7 @@ $(document).ready(function(){
         gameCounter=0;//maybe this resets the counter to zero
         $('#newGame').addClass('invisible');
         $('#winnerIs').text('');
+        console.log(gameCounter); 
     });// display newGame button
 
 });// end of game appliction function
